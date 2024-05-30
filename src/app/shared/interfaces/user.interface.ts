f9821ae73da9cd6b4fc829d08cc4febbf1ee41b9
Deleon Simoni <deleon.simoni>
2024-05-30 10:13:15 -0300

    acerto build prod

--- src/app/shared/interfaces/user.interface.ts
+++ src/app/shared/interfaces/user.interface.ts
@@ -1,6 +1,27 @@
 export interface User {
   _id: string;
   fullname: string;
+  email: string;
+  celular: string;
+  telefone: string;
+  socialname: string;
+  cpf: string;
+  rg: string;
+  rgEmissor: string;
+  passaporte: string;
+  dataNiver: string;
+  nacionalidade: string;
+  endereco: string;
+  bairro: string;
+  cep: string;
+  cidade: string;
+  estado: string;
+  cargo: string;
+  empresa: string;
+  deficiencia: string;
+  cor: string;
+  genero: string;
+
   createdAt: string;
   roles: string[];
   isAdmin: boolean;
